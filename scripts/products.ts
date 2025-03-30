20858d1ffaa3bfccc365add7aa6694e3c1e1634b
Ajax Rose <ajaxrose17@gmail.com>
2025-03-29 20:34:58 -0400

    TS catalog

--- scripts/products.ts
+++ scripts/products.ts
@@ -1,26 +1,158 @@
-/*Catalog of all products*/
+/* Catalog of all products */
 
 const products = [
+    /* ---- Tents ---- */
     {
-      id: "tent01",
-      name: "Mystic Tent",
-      price: 199.99,
-      category: "tents",
-      image: "tent.jpg",
+        id: "tent01",
+        name: "Enchanted Tent",
+        price: 209.99,
+        category: "tents",
+        image: "../images/enchanted-tents.png",
+        description: "Campfire tent made with durable materials and design that turns your ambience into an extraordinary and mesmerizing world you’ve never seen before.",
+        includes: [
+            "Tents with built-in LED lights",
+            "Compact carrying case",
+            "Design to fit up to 4 people"
+        ]
     },
     {
-      id: "backpack01",
-      name: "Explorer Backpack",
-      price: 89.99,
-      category: "backpacks",
-      image: "backpack.jpg",
+        id: "tent02",
+        name: "Nyx’s Veil Stargazer Tent",
+        price: 199.99,
+        category: "tents",
+        image: "../images/nyxs-tent.png",
     },
     {
-      id: "lantern01",
-      name: "Glow Lantern",
-      price: 49.99,
-      category: "lanterns",
-      image: "lantern.jpg",
+        id: "tent03",
+        name: "Wendigo's Whisper Shelter",
+        price: 249.99,
+        category: "tents",
+        image: "../images/wendigos-tent.png",
     },
-    // Add more here...
-  ];
+    /* ---- Sleeping Bags ---- */
+    {
+        id: "sleepbag01",
+        name: "Kraken’s Abyss Sleeping Bag",
+        price: 169.99,
+        category: "sleepingbags",
+        image: "../images/krakens-sleepingbag.png",
+    },
+    {
+        id: "sleepbag02",
+        name: "Phoenix Ember Recharge Sleeping Bag",
+        price: 129.99,
+        category: "sleepingbags",
+        image: "../images/phoenix-sleepingbag.png",
+    },
+    /* ---- Lighting ---- */
+    {
+        id: "light01",
+        name: "Will-o’-Wisp Phantom Lantern",
+        price: 49.99,
+        category: "lighting",
+        image: "../images/willowisp-lantern.png",
+    },
+    {
+        id: "light02",
+        name: "Cerberus Tri-Flame Tactical Flashlight",
+        price: 59.99,
+        category: "lighting",
+        image: "../images/cerberus-flashlight.png",
+    },
+    {
+        id: "light03",
+        name: "Selene’s Moonbeam Glow Stick",
+        price: 34.99,
+        category: "lighting",
+        image: "../images/selenes-flashlight.png",
+    },
+    /* ---- Jackets ---- */
+    {
+        id: "jacket01",
+        name: "Yeti’s Frostbound Expedition Jacket",
+        price: 229.99,
+        category: "jackets",
+        image: "../images/yetis-jacket.png",
+    },
+    {
+        id: "jacket02",
+        name: "Shadow Stalker Wraith Cloak",
+        price: 199.99,
+        category: "jackets",
+        image: "../images/shadow-jacket.png",
+    },
+    /* ---- Cookware ---- */
+    {
+        id: "cookware01",
+        name: "Mystic Campfire Cooking Set",
+        price: 87.99,
+        category: "cookware",
+        image: "../images/mystic-campfire-cook-set1.png",
+    },
+    {
+        id: "cookware02",
+        name: "Baba Yaga’s Iron Cauldron Cookware Set",
+        price: 89.99,
+        category: "cookware",
+        image: "../images/baba-yaga-cookset.png",
+    },
+    /* ---- First Aid Kits ---- */
+    {
+        id: "firstaid01",
+        name: "Selkie’s Seaside First Aid Kit",
+        price: 39.99,
+        category: "firstaid",
+        image: "../images/sea-firstaid-kit.png",
+    },
+    {
+        id: "firstaid02",
+        name: "Nessie’s Guardian First Aid Kit",
+        price: 42.99,
+        category: "firstaid",
+        image: "../images/nessie-firstaid-kit.png",
+    },
+    /* ---- Fire Starters ---- */
+    {
+        id: "fire01",
+        name: "Jörmungandr's Ember Coil",
+        price: 14.99,
+        category: "firestarters",
+        image: "../images/ember-coil.png",
+    },
+    {
+        id: "fire02",
+        name: "Mothman’s Shadow Spark",
+        price: 15.99,
+        category: "firestarters",
+        image: "../images/moth-firestarter.png",
+    },
+    {
+        id: "fire03",
+        name: "Mystic Campfire Kit",
+        price: 55.99,
+        category: "firestarters",
+        image: "../images/mystic-campfire-kit1.png",
+    },
+    /* ---- Repellents ---- */
+    {
+        id: "repel01",
+        name: "Mothman’s Nocturnal Guard Bug Spray",
+        price: 12.99,
+        category: "repellent",
+        image: "../images/moth-repel.png",
+    },
+    {
+        id: "repel02",
+        name: "Chupacabra Venom Mosquito Repellent Balm",
+        price: 9.99,
+        category: "repellent",
+        image: "../images/chup-repel.png",
+    },
+    {
+        id: "repel03",
+        name: "Banshee’s Wail Ultrasonic Pest Repeller",
+        price: 24.99,
+        category: "repellent",
+        image: "../images/banshee-repel.png",
+    },
+];
