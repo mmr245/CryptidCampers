2bf209e2eabc5253f8ba47c56cd481a2286887cc
Ajax Rose <ajaxrose17@gmail.com>
2025-03-29 20:12:31 -0400

    scripts folder and product array TS

---
+++ scripts/products.ts
@@ -0,0 +1,26 @@
+/*Catalog of all products*/
+
+const products = [
+    {
+      id: "tent01",
+      name: "Mystic Tent",
+      price: 199.99,
+      category: "tents",
+      image: "tent.jpg",
+    },
+    {
+      id: "backpack01",
+      name: "Explorer Backpack",
+      price: 89.99,
+      category: "backpacks",
+      image: "backpack.jpg",
+    },
+    {
+      id: "lantern01",
+      name: "Glow Lantern",
+      price: 49.99,
+      category: "lanterns",
+      image: "lantern.jpg",
+    },
+    // Add more here...
+  ];
